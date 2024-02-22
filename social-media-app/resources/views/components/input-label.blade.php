568052b79d5f8c2628c456fd3a456277261d9989
bishal <bi.gb.ak.cs.it@gmail.com>
2024-02-22 11:35:15 +0530

    start

---
+++ social-media-app/resources/views/components/input-label.blade.php
@@ -0,0 +1,6 @@
+@props(['value'])
+@vite('resources/css/app.css')
+
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
+    {{ $value ?? $slot }}
+</label>
